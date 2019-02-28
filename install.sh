cc7301225cac6b7386a3b900989600c22119c2b8
antononim <47603615+antononim@users.noreply.github.com>
2019-02-28 17:48:01 +0200

    correcting to installations

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 echo -e "\e[1m STARTING INSTALLATION... \e[0m"
 apt update
 pkg install python*
-pip install vk vk_api getpass > null
-pip3 install vk vk_api getpass > null
+pip install vk vk_api
+pip install getpass
+pip3 install vk vk_api
+pip install getpass
 echo alias vkill="python3 ~/vkill/vk_killer.py" >> ~/../usr/etc/bash.bashrc
 echo -e '\e[1;3;31m Done! \e[0m'
 rm null
